ee6efc29bc0add9208df9b55c472dcdaa96b6c77
vnthanh25 <vnthanh25@gmail.com>
2017-05-04 09:26:37 +0700

    Replace package for test.

--- SBMvc/src/main/java/com/bim/server/SbMvcApplication.java
+++ SBMvc/src/main/java/com/bim/server/SbMvcApplication.java
@@ -2,8 +2,30 @@ package com.bim.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
+
+/*
+@SpringBootConfiguration
+@ContextConfiguration(classes = SbMvcApplication.class)
+@Configuration
+@ComponentScan
+@EnableJpaRepositories
+@Import(RepositoryRestConfiguration.class)
+@EnableAutoConfiguration
+@PropertySource("application.yml")
+*/
+/*
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.bim.server.repository"})
+@ComponentScan(basePackages = {"com.bim.server.controller","com.bim.server.model"})
+@EnableWebMvc
+@PropertySources(value = {@PropertySource("classpath:application.yml")})
+@EntityScan(basePackages={"com.bim.server.model"})
+*/
+/*
+@Import(JpaConfig.class)
+@SpringBootApplication(scanBasePackages={"com.bim.server"})// same as @Configuration @EnableAutoConfiguration @ComponentScan
+*/
 @SpringBootApplication
 public class SbMvcApplication {
 
--- SBMvc/src/test/java/com/bim/server/SbMvcApplicationTests.java
+++ SBMvc/src/test/java/com/bim/server/SbMvcApplicationTests.java
@@ -1,12 +1,16 @@
-package com.example;
+package com.bim.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest
+@DataJpaTest
 public class SbMvcApplicationTests {
 
 	@Test
