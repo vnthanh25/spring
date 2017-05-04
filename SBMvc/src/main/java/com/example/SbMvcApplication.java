f73c58a3f1f4ff44840f2aa9cf02e4d1dd7c33b0
vnthanh25 <vnthanh25@gmail.com>
2017-05-04 07:18:00 +0700

    Created entity User.

--- SBMvc/src/main/java/com/example/SbMvcApplication.java
+++ SBMvc/src/main/java/com/example/SbMvcApplication.java
@@ -2,11 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SbMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbMvcApplication.class, args);
+		/*
+		new SpringApplicationBuilder(SbMvcApplication.class)
+        .profiles("prod")
+        .run(args);
+        */
 	}
 }
---
+++ SBMvc/src/main/java/com/example/model/User.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user", schema="public")
+public class User implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Integer age;
+	private double salary;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
