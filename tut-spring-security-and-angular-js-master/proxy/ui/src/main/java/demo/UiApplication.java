a56aa28102c82e1998e408917794cf1dd1e004c3
vnthanh25 <vnthanh25@gmail.com>
2017-05-10 16:02:25 +0700

    tut-spring-security-and-angular-js-master.

---
+++ tut-spring-security-and-angular-js-master/proxy/ui/src/main/java/demo/UiApplication.java
@@ -0,0 +1,50 @@
+package demo;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableZuulProxy
+public class UiApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.httpBasic().and()
+				.logout().and()
+				.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+					.anyRequest().authenticated()
+					.and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
+		}
+	}
+
+}
---
+++ tut-spring-security-and-angular-js-master/spring-session/ui/src/main/java/demo/UiApplication.java
@@ -0,0 +1,58 @@
+package demo;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class UiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.httpBasic().and()
+				.logout().and()
+				.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+					.anyRequest().authenticated().and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
+		}
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@RequestMapping("/token")
+	public Map<String,String> token(HttpSession session) {
+		return Collections.singletonMap("token", session.getId());
+	}
+
+}
