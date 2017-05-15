7c96fe46a068de8e75d71beea102bd59163a44ac
vnthanh25 <vnthanh25@gmail.com>
2017-05-15 08:10:16 +0700

    user_role table schema.

---
+++ tut-spring-security-and-angular-js-master/oauth2/authserver/user_role.sql
@@ -0,0 +1,14 @@
+﻿--DROP TABLE IF EXISTS user;
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username varchar(100) DEFAULT NULL,
+  password varchar(300) DEFAULT NULL,
+  enabled bit(1) NULL DEFAULT B'0'
+);
+
+--DROP TABLE IF EXISTS user_role;
+CREATE TABLE IF NOT EXISTS user_roles (
+  id SERIAL PRIMARY KEY,
+  username varchar(100) DEFAULT NULL,
+  role varchar(300) DEFAULT NULL
+);
